5e629232c5e420e1c4971152ef4728f36ec6654a
Iam-Ntwali <154070891+Iam-Ntwali@users.noreply.github.com>
2024-01-16 20:16:30 +0200

    some changes

--- coursera - Meta/functions.js
+++ coursera - Meta/functions.js
@@ -1,11 +1,12 @@
-console.log('connected');
+// console.log('connected');
 // loop thru arrays
 var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
 
 function listArrayItems(arr) {
   for (var i = 0; i < arr.length; i++) {
-    console.log(arr[i]) //display the array item where the index is euqal to i
+    console.log(`Color ${i + 1} in Colors Array is ${arr[i]}`) //display the array item where the index is euqal to i
   }
 }
+listArrayItems(colors);
 
-listArrayItems
+
