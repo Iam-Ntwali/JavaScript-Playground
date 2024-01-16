d8d23faacf4cca61661b5ba1eb73fedf117380f4
Iam-Ntwali <154070891+Iam-Ntwali@users.noreply.github.com>
2024-01-16 20:02:35 +0200

    some changes

---
+++ coursera - Meta/functions.js
@@ -0,0 +1,11 @@
+console.log('connected');
+// loop thru arrays
+var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
+
+function listArrayItems(arr) {
+  for (var i = 0; i < arr.length; i++) {
+    console.log(arr[i]) //display the array item where the index is euqal to i
+  }
+}
+
+listArrayItems
