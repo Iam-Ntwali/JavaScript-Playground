bdc0bb8532d79118b147640aafc39a7b193e0671
Iam-Ntwali <154070891+Iam-Ntwali@users.noreply.github.com>
2024-01-17 17:34:45 +0200

    some changes

--- coursera - Meta/functions.js
+++ coursera - Meta/functions.js
@@ -1,23 +1,25 @@
 // console.log('connected');
 // loop thru arrays
-var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
+// var colors = ['red', 'orange', 'yellow', 'green', 'blue', 'purple', 'pink'];
 
-function listArrayItems(arr) {
-  for (var i = 0; i < arr.length; i++) {
-    console.log(`Color ${i + 1} in Colors Array is ${arr[i]}`) //display the array item where the index is euqal to i
-  }
-}
-listArrayItems(colors);
+// function listArrayItems(arr) {
+//   for (var i = 0; i < arr.length; i++) {
+//     console.log(`Color ${i + 1} in Colors Array is ${arr[i]}`) //display the array item where the index is euqal to i
+//   }
+// }
+// listArrayItems(colors);
 
 // EXERCISES PRACTICE
-function letterFinder(word, match) {
-  for (let i = 0; i < word.length; i++) {
-    if (word[i] === match) {
-      console.log(`found the match ${match} at Index ${i}`);
-    } else {
-      console.log(`--No match found at ${i}`);
-    }
-  }
-}
+// function letterFinder(word, match) {
+//   for (let i = 0; i < word.length; i++) {
+//     if (word[i] === match) {
+//       console.log(`found the match ${match} at Index ${i}`);
+//     } else {
+//       console.log(`--No match found at ${i}`);
+//     }
+//   }
+// }
 
-letterFinder('test', 't');
+// letterFinder('test', 't');
+
+// OBJECTS
